18e2f8aa10317a72f8d8fd3f25900972991d322b
root <root@www.cryptodipsignals.com>
2021-07-27 18:34:44 -0400

    updated bkcrack_auto.py so that it takes the path to bkcrack binary

--- tools/bkcrack_auto.py
+++ tools/bkcrack_auto.py
@@ -4,12 +4,15 @@ import sys
 import os
 import subprocess
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
 	print("You're missing a file path to the zip files")
-elif len(sys.argv) > 2:
+	print("./bkcrack_auto.py <path_to_bkcrack> <path_to_zips>")
+elif len(sys.argv) > 3:
 	print("You have too many arguments")
+	print("./bkcrack_auto.py <path_to_bkcrack> <path_to_zips>")
 
-zip_path = sys.argv[1]
+bkcrack_path = sys.argv[1]
+zip_path = sys.argv[2]
 ext = ('.zip', '.ZIP')
 
 for files_name in os.listdir(zip_path):
@@ -44,13 +47,12 @@ for files_name in os.listdir(zip_path):
 		# In order of speed to crack
 		for svg_item in svg_array:
 			print("Finding key for svg %s in %s" % (svg_item, files_name))
-			print('bkcrack -C %s/%s -c %s -p %s' % (zip_path, files_name, svg_item, 'file_search/svg.txt'))
-			exp_command = 'bkcrack -C %s/%s -c %s -p %s' % (zip_path, files_name, svg_item, 'file_search/svg.txt')
+			exp_command = '%s/bkcrack -C %s/%s -c %s -p %s' % (bkcrack_path, zip_path, files_name, svg_item, 'file_search/svg.txt')
 			key_lines = subprocess.check_output(exp_command, shell=True).split('\n')
 			key = key_lines[-2]
 			if 'could' not in key:
 				print("Found key %s, attempting to find the password up to 12 char" % key)
-				pwd_command = 'bkcrack -k %s -r 12 ?a' % key
+				pwd_command = '%s/bkcrack -k %s -r 12 ?a' % (bkcrack_path, key)
 				pwd_lines = subprocess.check_output(pwd_command, shell=True).split('\n')
 				pwd = pwd_lines[-2]
 				if 'Could' not in pwd:
@@ -58,7 +60,7 @@ for files_name in os.listdir(zip_path):
 				else:
 					print("Could not find password changing the zip password to 'password'")
 					new_filename = "%s/%s.%s.zip" % (zip_path, files_name, key.replace(" ", "_"))
-					change_command = 'bkcrack -C %s -k %s -U %s password' % (files_Name, key, new_filename)
+					change_command = '%s/bkcrack -C %s -k %s -U %s password' % (bkcrack_path, files_Name, key, new_filename)
 					subprocess.check_output(change_command, shell=True).split('\n')					
 					
 			else:
@@ -66,13 +68,12 @@ for files_name in os.listdir(zip_path):
 
 		for jpg_item in jpg_array:
 			print("Finding key for jpg %s in %s" % (jpg_item, files_name))
-			print('bkcrack -C %s/%s -c %s -p %s' % (zip_path, files_name, svg_item, 'file_search/jpg.txt'))
-			exp_command = 'bkcrack -C %s/%s -c %s -p %s' % (zip_path, files_name, jpg_item, 'file_search/jpg.txt')
+			exp_command = '%s/bkcrack -C %s/%s -c %s -p %s' % (bkcrack_path, zip_path, files_name, jpg_item, 'file_search/jpg.txt')
 			key_lines = subprocess.check_output(exp_command, shell=True).split('\n')
 			key = key_lines[-2]
 			if 'could' not in key:
 				print("Found key %s, attempting to find the password up to 12 char" % key)
-				pwd_command = 'bkcrack -k %s -r 12 ?a' % key
+				pwd_command = '%s/bkcrack -k %s -r 12 ?a' % (bkcrack_path, key)
 				pwd_lines = subprocess.check_output(pwd_command, shell=True).split('\n')
 				pwd = pwd_lines[-2]
 				if 'Could' not in pwd:
@@ -80,7 +81,7 @@ for files_name in os.listdir(zip_path):
 				else:
 					print("Could not find password changing the zip password to 'password'")
 					new_filename = "%s/%s.%s.zip" % (zip_path, files_name, key.replace(" ", "_"))
-					change_command = 'bkcrack -C %s -k %s -U %s password' % (files_Name, key, new_filename)
+					change_command = '%s/bkcrack -C %s -k %s -U %s password' % (bkcrack_path, files_Name, key, new_filename)
 					subprocess.check_output(change_command, shell=True).split('\n')					
 					
 			else:
@@ -88,13 +89,12 @@ for files_name in os.listdir(zip_path):
 
 		for png_item in png_array:
 			print("Finding key for png %s in %s" % (png_item, files_name))
-			print('bkcrack -C %s/%s -c %s -p %s' % (zip_path, files_name, svg_item, 'file_search/png.txt'))
-			exp_command = 'bkcrack -C %s/%s -c %s -p %s' % (zip_path, files_name, png_item, 'file_search/png.txt')
+			exp_command = '%s/bkcrack -C %s/%s -c %s -p %s' % (bkcrack_path, zip_path, files_name, png_item, 'file_search/png.txt')
 			key_lines = subprocess.check_output(exp_command, shell=True).split('\n')
 			key = key_lines[-2]
 			if 'could' not in key:
 				print("Found key %s, attempting to find the password up to 12 char" % key)
-				pwd_command = 'bkcrack -k %s -r 12 ?a' % key
+				pwd_command = '%s/bkcrack -k %s -r 12 ?a' % (bkcrack_path, key)
 				pwd_lines = subprocess.check_output(pwd_command, shell=True).split('\n')
 				pwd = pwd_lines[-2]
 				if 'Could' not in pwd:
@@ -102,7 +102,7 @@ for files_name in os.listdir(zip_path):
 				else:
 					print("Could not find password changing the zip password to 'password'")
 					new_filename = "%s/%s.%s.zip" % (zip_path, files_name, key.replace(" ", "_"))
-					change_command = 'bkcrack -C %s -k %s -U %s password' % (files_Name, key, new_filename)
+					change_command = '%s/bkcrack -C %s -k %s -U %s password' % (bkcrack_path, files_Name, key, new_filename)
 					subprocess.check_output(change_command, shell=True).split('\n')					
 					
 			else:
